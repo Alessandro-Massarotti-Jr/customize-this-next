7604a24d29a3c2b9eb25d7e686083b0993fab03d
Alessandro-Massarotti-junior <alemassajr@hotmail.com>
2022-11-13 12:59:07 -0300

    add header css modules

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,3 +1,21 @@
-export default function Header(){
-    return(<h1>Header</h1>)
+import Image from "next/image"
+import Link from "next/link"
+import styles from "../styles/Header.module.css"
+
+export default function Header() {
+    return (
+        <header className={styles.header}>
+            <Image src={"/vercel.svg"} alt="image" width={80} height={80} />
+            <nav>
+                <ul>
+                    <li>
+                        <Link href={'/'}>Login</Link>
+                    </li>
+                    <li>
+                        <Link href={'/'}>Cadastre-se</Link>
+                    </li>
+                </ul>
+            </nav>
+        </header>
+    )
 }
